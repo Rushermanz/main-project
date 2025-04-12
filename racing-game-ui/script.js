94db7be3550f2c2362bd126afb24e8f8678dff12
Rushermanz <128926240+Rushermanz@users.noreply.github.com>
2025-04-12 19:04:24 +0530

    Initial commit

---
+++ racing-game-ui/script.js
@@ -0,0 +1,16 @@
+function openProfile() {
+    alert("Open Profile Section (modal)");
+  }
+  
+  function openPlay() {
+    alert("Go to Track Selection");
+  }
+  
+  function openSettings() {
+    alert("Settings popup here!");
+  }
+  
+  function quitGame() {
+    alert("Thanks for playing!");
+  }
+  
